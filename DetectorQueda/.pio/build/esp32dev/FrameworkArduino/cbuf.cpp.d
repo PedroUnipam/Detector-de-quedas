decc25d2ee22dbb188d56dee3578c3a166b9bbfe
Pedro <pedrohsousa82@gmail.com>
2025-10-31 16:09:10 -0300

    Revert "teste"
    This reverts commit 595afa511f4d6230c94b3d42bf5ee5abd291b85f.

--- DetectorQueda/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
