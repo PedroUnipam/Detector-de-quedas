595afa511f4d6230c94b3d42bf5ee5abd291b85f
Pedro <pedrohsousa82@gmail.com>
2025-10-31 15:47:57 -0300

    teste

---
+++ DetectorQueda/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
